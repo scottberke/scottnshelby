adccb6bbe44b6a205f6fd48d2cae562344e32e61
scottberke <scottberke@gmail.com>
2015-02-21 16:33:52 -0700

    uses partials

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
+
+	attr_accessor :first_name, :last_name, :email
+
   def welcome
   end
 
   def contact
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
+  root 'static_pages#welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
